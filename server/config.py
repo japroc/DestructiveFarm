cfe92cf34e27ef503dcfbf5d1f9861ef97372fb2
root <root@localhost.localdomain>
2018-11-10 04:17:31 -0500

    Configs for RuCTFE

--- server/config.py
+++ server/config.py
@@ -1,18 +1,23 @@
+TEAMS = dict()
+for i in range(1, 255 + 1):
+    TEAMS['Team #{}'.format(i)] = '10.60.{}.0'.format(i)
+for i in range(0, 41 + 1):
+    TEAMS['Team #{}'.format(256 + i)] = '10.61.{}.0'.format(i)
+
 CONFIG = {
     # Don't forget to remove the old database (flags.sqlite) before each competition.
 
     # The clients will run sploits on TEAMS and
     # fetch FLAG_FORMAT from sploits' stdout.
-    'TEAMS': {'Team #{}'.format(i): '10.0.0.{}'.format(i)
-              for i in range(1, 29 + 1)},
-    'FLAG_FORMAT': r'[A-Z0-9]{31}=',
+    'TEAMS': TEAMS,
+    'FLAG_FORMAT': r'\w{31}=',
 
     # This configures how and where to submit flags.
     # The protocol must be a module in protocols/ directory.
     # RuCTF(E) and VolgaCTF checksystems are supported out-of-the-box.
 
     'SYSTEM_PROTOCOL': 'ructf_tcp',
-    'SYSTEM_HOST': '127.0.0.1',
+    'SYSTEM_HOST': '10.10.10.10', # 10.10.10.3
     'SYSTEM_PORT': 31337,
 
     # 'SYSTEM_PROTOCOL': 'ructf_http',
@@ -27,11 +32,11 @@ CONFIG = {
     # FLAG_LIFETIME seconds ago will be skipped.
     'SUBMIT_FLAG_LIMIT': 50,
     'SUBMIT_PERIOD': 5,
-    'FLAG_LIFETIME': 5 * 60,
+    'FLAG_LIFETIME': 15 * 60,
 
     # Password for the web interface. This key will be excluded from config
     # before sending it to farm clients.
-    'SERVER_PASSWORD': '1234',
+    'SERVER_PASSWORD': '1qaz@WSX',
 
     # Use authorization for API requests
     'ENABLE_API_AUTH': False,
